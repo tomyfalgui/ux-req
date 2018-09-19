94b1568e1b43c61386ae1135657d840576bd88c8
tomyfalgui <tomyfalguiiii@yahoo.com>
2018-09-19 11:39:53 +0800

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'UX Society',
+  },
+  plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-styled-components`],
+}
---
+++ src/components/layout.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet title={data.site.siteMetadata.title}>
+          <html lang="en" />
+        </Helmet>
+        {children}
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
